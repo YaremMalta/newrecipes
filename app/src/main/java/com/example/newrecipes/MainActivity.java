498e9dcaaff8cfcab1742e1ac84bba5a7ce8f147
YaremMalta <111348773+YaremMalta@users.noreply.github.com>
2024-06-10 14:18:48 +0300

    first commit

---
+++ app/src/main/java/com/example/newrecipes/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.newrecipes;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/newrecipes/data/pojo/MealsResponse.kt
@@ -0,0 +1,5 @@
+package com.example.newrecipes.data.pojo
+
+data class MealsResponse(
+    val meals: List<Meal>
+)
